162c2cf189427358851b7b7fc7b213cb0daafab0
Cristina Amoedo <gizanakris@gmail.com>
2023-10-06 12:53:11 +0200

    Creación de dos botones e iniciándome en el uso de las constraints

--- KodekoSwiftApp/KodekoSwiftApp/ViewController.swift
+++ KodekoSwiftApp/KodekoSwiftApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Button1: UIButton!
+    
+    @IBOutlet weak var Button2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+      if sender.title(for: .normal) == "X" {
+        sender.setTitle("A very long title for this button", for: .normal)
+      } else {
+        sender.setTitle("X", for: .normal)
+      }
+    }
     }
 
+    
 
-}
 
